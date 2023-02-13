f7df69cc7f2e5822616dbe9de270a0265f89d182
LHENI <lheni644@gmail.com>
2023-02-13 14:53:55 +0100

    first

---
+++ src/Cone.java
@@ -0,0 +1,7 @@
+
+public class Cone {
+    public double calcVolume(double radium,double height) {
+        double volume= 1.0/3.0 * Math.pow (radium,2) * Math.PI * height;
+        return volume;
+    }    
+}
---
+++ src/MainConsole.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MainConsole {
+    public MainConsole() {
+        System.out.println("Feladat 302");
+        String radiusStr = input("Sugár:  ");
+        String heightStr = input("Magasság:");
+        double radius = Double.parseDouble(radiusStr);
+        double height = Double.parseDouble(heightStr);
+        Cone cone = new Cone();
+        System.out.println(cone.calcVolume(radius, height));
+    }
+    public String input(String msg){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(msg + " ");
+        String text = scanner.nextLine();
+        return text;
+    }
+
+}
